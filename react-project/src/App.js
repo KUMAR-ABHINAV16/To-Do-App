f7fa89361ecca4271bee74c758a45401791a4627
unknown <abhinav69596959@gmail.com>
2022-07-10 08:11:30 +0530

    todo

---
+++ react-project/src/App.js
@@ -0,0 +1,31 @@
+
+
+import './App.css';
+import { useState } from 'react';
+import React from 'react';
+import Form from "./comp/Form"
+import Todolist from './comp/Todolist';
+
+
+
+function App() {
+
+const [input,setInput] = useState("");
+const[todos,setTodo] = useState([]);
+
+
+  return (
+
+ <div className='main'>
+    <div className="App">
+      <h1 className='heading'> TO-DO </h1>
+      <div className='stick'>
+        <Form input={input} setInput={setInput} todos={todos} setTodo={setTodo}/>
+        <Todolist todos={todos} setTodo={setTodo}/>
+        </div>
+    </div>
+  </div>
+  );
+}
+
+export default App;
